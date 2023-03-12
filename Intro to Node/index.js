25dbbf12594be9781ee0e543f77e7dbed90a2e6a
Nikola Milanović <nikola.milanovic.dev@gmail.com>
2023-03-12 13:50:33 +0100

    Intro to Node completed

--- Intro to Node/index.js
+++ Intro to Node/index.js
@@ -1,3 +1,14 @@
-const fs = require("fs");
+/* Internal Node.js module */
 
-fs.copyFileSync("file1.txt", "file2.txt");
+// const fs = require("fs");
+
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+/* Externam Node.js module */
+// const superheroes = require("superheroes");
+// const supervillains = require("supervillains");
+
+// var mySuperheroName = superheroes.random();
+// var mySupervillainName = supervillains.random();
+
+// console.log(mySuperheroName + " vs " + mySupervillainName);
